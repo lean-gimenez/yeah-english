2accd79318512e6a87e80931e3918272a88e3738
Lean <gimenez.leandro.j@gmail.com>
2021-11-04 22:08:03 -0300

    nesting + mixins + mejoras en diseño + intento de agregado de level.js fallido, pero ya saldrá + todo marcha bien, Milhouse

---
+++ level.js
@@ -0,0 +1,16 @@
+/*
+
+let send = document.getElementById("submit");
+let user = document.getElementById("name")
+
+send.onclick = formSent();
+
+function formSent(event){
+    if(user.length == 0){
+        alert("Seems like you've missed some spots :/")
+    } else {
+        alert("All set!")
+    }
+}
+
+*/
