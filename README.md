17aae409d346732e08e175dfeaf7696a93391e01
Lean <gimenez.leandro.j@gmail.com>
2021-11-19 01:11:54 -0300

    agregado de js para level.html + subrayado de links en map.html + contact links

---
+++ README.md
@@ -0,0 +1,23 @@
+# WELCOME TO YEAH! ENGLISH
+
+Este es un sitio web diseñado para acercar el inglés a más personas curiosas. Creado por Leandro Julián Giménez, estudiante de CoderHouse, licenciado en Letras, profesor de inglés y matemática, diseñador.
+
+El sitio está totalmente escrito en inglés y se planea reformarlo para que el usuario pueda acceder a ella en el idioma que requiera (excepto en el test de nivel, claro).
+
+---
+
+## ESTRUCTURA Y DISEÑO
+
+Para construir este sitio web se utilizó
+
+* HTML
+* CSS
+* Frameworks: *Bootstrap & SASS*
+* XML para el sitemap
+* Metodología BEM para la organización de clases
+
+---
+
+## AGRADECIMIENTOS
+
+Quiero agradecer a la profesora **Yanel Ricarte** y al tutor **Julián Preiss**. Súper claras sus explicaciones. Especialmente Julián, que dedicó mucho tiempo a la corrección y mejora del diseño para optimizar del sitio.
--- level.js
+++ level.js
@@ -1,16 +1,43 @@
-/*
+function formSent(){
+    
+    //user name
+    let name = document.getElementById("name");
 
-let send = document.getElementById("submit");
-let user = document.getElementById("name")
+    //user e-mail
+    let email = document.getElementById("email");
 
-send.onclick = formSent();
+    //total of questions
+    let total = 5;
 
-function formSent(event){
-    if(user.length == 0){
-        alert("Seems like you've missed some spots :/")
-    } else {
-        alert("All set!")
+    //score
+    let score = 0;
+
+    //reference to form
+    let form = document.forms["levelForm"];
+
+    //array of answers
+    let answers = ["c", "c", "a", "d", "b"];
+
+    //for: var i se inicializa en 1;
+    //debe ser menor que el total;
+    //al terminar el loop, incrementa var
+    for(let i = 1; i <= total; i++){
+
+        if(form["q" + i].value === null || form["q" + i].value === ""){
+            alert("Please, answer question " + i);
+            return false;
+
+        } else {
+
+            if(form["q" + i].value === answers[i - 1]){
+                score++;
+            }
+        }
     }
-}
 
-*/
+    
+    let results = document.getElementById("results");
+    results.innerHTML = '<div class="p-4"><h3><span>' + name.value + '</span>, your score is <span>' + score + '</span></h3> </div> <div class="p-2"> <h5>List of levels</h5> <ul><li>1 point = Elementary</li><li>2 points = Pre-Intermediate</li><li>3 points = Intermediate</li><li>4 points = Upper-Intermediate</li><li>5 points = Advanced</li></ul></div>';
+
+    return false;
+}
